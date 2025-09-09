7af4dd0d81a1b5a810626ab4cb352b64f05f3554
YuriAquille <202506032761@alunos.estacio.br>
2025-09-09 20:32:32 +0000

    Definição de posição

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,3 +4,20 @@
 
 int main() {
     int tabuleiro[TAM][TAM] = {0};  // inicializa o tabuleiro com 0
+
+    //Manual dos navios
+
+    //Navio vertical 3 posições
+
+    int navioverticalX = 2; //coluna fixa 
+    int navioverticalY = 4; // linha inicial
+    int tamanhovertical = 3;
+
+    //navio horizontal 4 posições
+
+    int navioHorizontalX = 6; // linha fixa
+    int navioHorizontalY = 1; // coluna 
+    int tamanhohorizontal = 4;
+
+    printf("--- BATALHA NAVAL - NIVEL NOVATO ---") ;
+    
